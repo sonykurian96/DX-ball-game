4cefc5dbc458d7d0bf8dc32a47b921355d0b6c3f
sonykurian96 <67534990+sonykurian96@users.noreply.github.com>
2020-09-06 20:59:23 +0530

    Music Class

---
+++ Music.java
@@ -0,0 +1,26 @@
+package dx_ball;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class Music
+{
+    void playmusic(String musiclocation)
+    {
+        try{
+            File musicpath = new File(musiclocation);
+            if(musicpath.exists())
+            {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicpath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
